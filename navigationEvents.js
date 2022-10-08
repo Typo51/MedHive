74f40269fc87f880b5e61e7fdb15193c2d4a10fc
Rouxerer <105259711+Rouxerer@users.noreply.github.com>
2022-10-09 02:26:51 +0800

    Add files via upload

---
+++ navigationEvents.js
@@ -0,0 +1,10 @@
+
+function sidebar(){
+ var sbarContainer = document.getElementById("sbarContainer");
+ sbarContainer.classList.toggle("active");
+}
+
+function search(){
+    var searchBtn = document.getElementById("searchBtn");
+    searchBtn.classList.toggle("active");
+   }
